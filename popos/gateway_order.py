f484d4df23f9194b22636f08c1d9109880243dce
Hykilpikonna <me@hydev.org>
2022-08-11 12:40:54 -0400

    [F] Fix capitalization

--- popos/gateway_order.py
+++ popos/gateway_order.py
@@ -5,65 +5,65 @@ from typing import List, Any
 
 
 class GatewayOrderItem:
-    item_id: int
-    item_barcode: str
-    goods_name: str
-    goods_link: str
-    property: str
-    user_remark: str
-    real_count: int
-    unit_price: float
-    goods_pic: str
-    status_id: int
-    ordered_time: int
-    warehouse_id: int
-    warehouse_name: str
-    item_type: int
-    goods_code: str
+    ItemId: int
+    ItemBarcode: str
+    GoodsName: str
+    GoodsLink: str
+    Property: str
+    UserRemark: str
+    RealCount: int
+    UnitPrice: float
+    GoodsPic: str
+    StatusId: int
+    OrderedTime: int
+    WarehouseId: int
+    WarehouseName: str
+    ItemType: int
+    goodsCode: str
     sku: str
-    recharge_type: int
-    rebate_status: int
-    buyable_service_ext_list: List[Any]
-    open_packaging: int
+    rechargeType: int
+    rebateStatus: int
+    buyableServiceExtList: List[Any]
+    openPackaging: int
     refund: int
-    additional_all_photo_list: List[Any]
-    status_name: str
-    additional_service_video_url: None
-    has_hd_qc_video_service: int
+    additionalAllPhotoList: List[Any]
+    statusName: str
+    additionalServiceVideoUrl: None
+    hasHdQcVideoService: int
 
 
 class GatewayOrderOpList:
-    name: str
-    code: int
-    level: int
+    Name: str
+    Code: int
+    Level: int
 
 
 class GatewayOrder:
-    order_pkg_type: int
-    order_pkg_no: str
-    order_pkg_id: int
-    status_id: int
-    status_name: str
-    shop_source: str
-    op_list: List[GatewayOrderOpList]
-    items: List[GatewayOrderItem]
-    purchaser_name: str
-    purchaser_avatar: str
-    order_state: str
-    purchaser_id: str
-    spu_code: str
+    OrderPkgType: int
+    OrderPkgNo: str
+    OrderPkgId: int
+    StatusId: int
+    StatusName: str
+    ShopSource: str
+    OpList: List[GatewayOrderOpList]
+    Items: List[GatewayOrderItem]
+    purchaserName: str
+    purchaserAvatar: str
+    orderState: str
+    purchaserId: str
+    spuCode: str
     currency: str
-    order_status: int
-    source_icon: str
-    source_type: int
-    mall_delivery_name: str
-    total_price: float
-    total_freight: int
-    pay_price: float
-    paypal_check: bool
-    order_can_delete: bool
-    have_rebate_goods: bool
-    order_add_service: List[Any]
-    currency_symbol: str
-    total_refund: int
-    consultation_flag: bool
+    orderStatus: int
+    sourceIcon: str
+    sourceType: int
+    MallDeliveryName: str
+    TotalPrice: float
+    TotalFreight: int
+    PayPrice: float
+    paypalCheck: bool
+    orderCanDelete: bool
+    haveRebateGoods: bool
+    orderAddService: List[Any]
+    currencySymbol: str
+    totalRefund: int
+    consultationFlag: bool
